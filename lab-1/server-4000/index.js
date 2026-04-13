2f8d9c02527ead77e9fdd536f6ed010d02122f7d
unterrrkunft <114381125+unterrrkunft@users.noreply.github.com>
2026-04-13 09:13:11 +0300

    Add files via upload
    Added endpoint to serve support.js file.

--- lab-1/server-4000/index.js
+++ lab-1/server-4000/index.js
@@ -1,1 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+
+// API для fetch
+app.get("/messages", (req, res) => {
+  res.json({
+    message: "No new support messages"
+  });
+});
+
+// ОЦЕ ТРЕБА ДОДАТИ
+app.get("/support.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "support.js"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Support server running on http://localhost:${PORT}`);
+});
