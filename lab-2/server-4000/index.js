40bca91a86113feb64b667ba8e0553121a3da882
unterrrkunft <114381125+unterrrkunft@users.noreply.github.com>
2026-04-16 06:02:19 +0300

    Add files via upload

---
+++ lab-2/server-4000/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+
+// API для fetch
+app.get("/messages", (req, res) => {
+  res.json({
+    message: "No new support messages"
+  });
+});
+
+app.get("/support.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "support.js"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Support server running on http://localhost:${PORT}`);
+});
